f111ce0e644a502385bda58ee20805df652045ab
prahantsingh124 <prashantsingh12012@gmail.com>
2022-06-21 20:05:02 +0530

    minor fixes

--- src/main/resources/static/final_page.js
+++ src/main/resources/static/final_page.js
@@ -1,30 +1,28 @@
-fetch("writeData.json").then((data)=>{
-    return data.json();
-  }).then((obj)=>{
-     console.log(obj);
-       let tableData="";
-       var values=obj;
-       //const str = JSON.stringify(obj, undefined, 2);
-       for( i=0;i<values.length;i++){ 
-         var object = JSON.parse(JSON.stringify(values[i]));
-           if(object!="null"){
-             tableData+=
-             ` <tr>
-                 <td>${object["Installment No."]}</td>
-                 <td>${object["Stage Number"]}</td>
-                 <td>${object["Installment Due Date"]}</td>
-                 <td>${object["Installment Amount"]}</td>
-                 <td>${object["Interest Rate"]}</td>
-                 <td>${object["Component 1 (Principal)"]}</td>
-                 <td>${object["Component 2 (Interest)"]}</td>
-               <!--  <td>${object["Component 3 (Fees)"]}</td>
-                 <td>${object["Component 4 (Insurance Premium)"]}</td> -->
-                 <td>${object["Opening Balance"]}</td>
-                 <td>${object["Closing Balance"]}</td>
-             </tr>`;
-           }
-       }
-       document.getElementById("table_body").
-       innerHTML=tableData;
-  })
+fetch("output.json").then((data)=>{
+  return data.json();
+}).then((obj)=>{
+   console.log(obj);
+     let tableData="";
+     var values=obj;
+     //const str = JSON.stringify(obj, undefined, 2);
+     for( i=0;i<values.length;i++){ 
+       var object = JSON.parse(JSON.stringify(values[i]));
+         if(object!="null"){
+           tableData+=
+           ` <tr>
+               <td>${object["Installment Number"]}</td>
+               <td>${object["Stage Number"]}</td>
+               <td>${object["Installment Due Date"]}</td>
+               <td>${object["Installment Amount"]}</td>
+               <td>${object["Interest Rate"]}</td>
+               <td>${object["Current Principal"]}</td>
+               <td>${object["Current Interest"]}</td>
+               <td>${object["Current Opening Balance"]}</td>
+               <td>${object["Current Closing Balance"]}</td>
+           </tr>`;
+         }
+     }
+     document.getElementById("table_body").
+     innerHTML=tableData;
+})
 
